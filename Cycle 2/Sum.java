f2622366ae85eb5770ac6b7e2dc600815157b40d
Merin-Mary-Shibu <85628644+Merin-Mary-Shibu@users.noreply.github.com>
2022-01-26 01:00:48 +0530

    Create Sum.java

---
+++ Cycle 2/Sum.java
@@ -0,0 +1,19 @@
+import java.util.*;
+class Main 
+{
+    public static void main (String args[])
+    {
+        int sum=0,n,i;
+        Scanner obj = new Scanner(System.in);
+        System.out.println("Enter the number of elements in the array: ");
+        n=obj.nextInt();
+        int[]a=new int[50];
+        System.out.println("Enter the array elements: ");
+        for(i=0;i<n;i++)
+        {
+           a[i]=obj.nextInt(); 
+           sum=sum+a[i];
+        }
+        System.out.println("Sum of the array elements = "+sum);
+    }
+}
