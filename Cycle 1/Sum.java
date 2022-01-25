dc2f8dc5bf6a9a9583874888ce1743042cf722f7
Merin-Mary-Shibu <85628644+Merin-Mary-Shibu@users.noreply.github.com>
2022-01-25 23:59:45 +0530

    Update Sum.java

--- Cycle 1/Sum.java
+++ Cycle 1/Sum.java
@@ -1,1 +1,14 @@
-
+import java.util.*;
+class Sum
+{
+    public static void main(String args[])
+    {
+        int a,b,sum;
+        Scanner obj = new Scanner(System.in);
+        System.out.println("Enter the two numbers to be added");
+        a=obj.nextInt();
+        b=obj.nextInt();
+        sum=a+b;
+        System.out.println("Result = "+sum);
+    }
+}
