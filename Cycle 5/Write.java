39dcc95a8ccda369887a93a3b471a2242bb1b086
Merin-Mary-Shibu <85628644+Merin-Mary-Shibu@users.noreply.github.com>
2022-02-26 19:39:44 +0530

    Create Write.java

---
+++ Cycle 5/Write.java
@@ -0,0 +1,29 @@
+import java.io.*;
+public class Write
+{
+    public static void main(String args[]) throws Exception
+    {
+        int i=0,c=0,l=1,w=0;
+        try
+        {
+            FileInputStream f = new FileInputStream("t2.txt");
+            while ((i=f.read())!=-1)
+            {
+              c++;
+              if(i=='\n')
+                  l++;
+              if(i== ' '|| i=='\n')
+                  w++;
+            }
+            System.out.println("No of characters = "+c);
+            System.out.println("No of lines = "+l);
+            System.out.println("No of words = "+w);
+            f.close();
+        }
+        catch(Exception e)
+        {
+            System.out.println(e);
+        }
+    }
+  }
+  
